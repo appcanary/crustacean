06cb98dbd897ff28e93ed87be7a5105175eac169
Max Veytsman <maxim@ontoillogical.com>
2015-06-30 17:37:49 -0700

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject appcanary/crustacean "0.1.0-SNAPSHOT"
+(defproject appcanary/crustacean "0.1.0"
   :description "CRU operations & migrations for datomic"
   :url "http://example.com/FIXME"
   :license {:name "Apache License Version 2.0"
