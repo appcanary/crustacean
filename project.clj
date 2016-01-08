5e721be73b68eb3d2217f575ad0ff896039c31bb
Max Veytsman <maxim@ontoillogical.com>
2016-01-08 15:38:39 -0500

    Dependency updates
    Bump schema, potemkin
    Separate out profiles for diff clojure versions

--- project.clj
+++ project.clj
@@ -3,18 +3,18 @@
   :url "http://example.com/FIXME"
   :license {:name "Apache License Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-
-                 [prismatic/schema "1.0.1"]
+  :dependencies [[prismatic/schema "1.0.4"]
                  [prismatic/plumbing "0.5.2"]
 
                  [org.flatland/ordered "1.5.3"]
                  
-                 [com.datomic/datomic-free "0.9.5186"]
+                 [com.datomic/datomic-free "0.9.5344"]
                  [io.rkn/conformity "0.3.3" :exclusions [com.datomic/datomic-free]]
                  [datomic-schema "1.2.2"]
-                 [potemkin "0.4.1"]]
+                 [potemkin "0.4.3"]]
   :profiles {:repl {:main user}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}
              :dev {:source-paths ["dev"]
                    :plugins [[lein-midje "3.1.3"]]
                    :dependencies [[midje "1.7.0-beta1"]]}})
