243ad809d4ab39c974b00a6ff3af4b9ca8d274fa
Max Veytsman <maxim@ontoillogical.com>
2016-01-18 17:54:35 -0500

    Moved the db funcs into their own namespace

--- src/crustacean/core.clj
+++ src/crustacean/core.clj
@@ -206,40 +206,10 @@
 
 
 ;; TODO: maybe call this "invalid"
-(defn ->malformed?*
-  "The malformed? database function for a given entity"
-  [entity input-schema*]
-  (pr-str `{:lang :clojure
-            :requires [[schema.core]]
-            :params [input#]
-            :code (let [checker#  (schema.core/checker (eval ~input-schema*))]
-                    (checker# input#))}))
-
-
 (defn ->malformed?
   "The `malformed?` function for a given entity"
   [{:keys [input-schema] :as model}]
   (s/checker input-schema))
-
-(defn ->exists?*
-  "The `exists?` database function for a given entity"
-  [entity]
-  (pr-str `{:lang :clojure
-            :params [db# input#]
-            :code (let [unique-key-pairs# (->> [~@(unique-fields entity)]
-                                               (filter #(contains? input# (keyword %)))
-                                               (map (juxt #(keyword ~(:namespace entity) %) #(get input# (keyword %)))))
-
-                        composite-key-pairs# (->> ~(:composite-keys entity)
-                                                  (filter (fn [[a# b#]] (and (contains? input# a#) (contains? input# b#))))
-                                                  (map (fn [[a# b#]]
-                                                         [(keyword ~(:namespace entity) (name a#)) (get input# a#)
-                                                          (keyword ~(:namespace entity) (name b#)) (get input# b#)])))]
-                    (seq (concat
-                          (d/q {:find '[[~'?e ...]] :in '[~'$ [[~'?attr ~'?value]]] :where '[[~'?e ~'?attr ~'?value]]} db# unique-key-pairs#)
-                          (mapcat (fn [[attr1# value1# attr2# value2#]]
-                                    (d/q {:find '[[~'?e ...]] :in '[~'$ ~'?value1 ~'?value2] :where `[[~'~'?e ~attr1# ~'~'?value1] [~'~'?e  ~attr2# ~'~'?value2]]} db# value1# value2#))
-                                  composite-key-pairs#))))}))
 
 (defn ->exists?
   "The `exits?` function for a given entity"
@@ -249,46 +219,7 @@
 
 ;; Entity Creation
 
-(defn ->create*
-  "The `create` database function for a given entity"
-  [entity]
-  (let [fields (:fields entity)
-        defaults (:defaults entity)
-        backrefs (:backrefs entity)]
-    (pr-str `{:lang :clojure
-              :params [db# id# input#]
-              :code
-              (if-let [malformed# (d/invoke db# (keyword ~(:namespace entity) "malformed?") input#)]
-                (throw (IllegalArgumentException. (str malformed#)))
 
-                (if (d/invoke db# (keyword ~(:namespace entity) "exists?") db# input#)
-                  (throw (IllegalStateException. "entity already exists"))
-
-                  (vector (into {:db/id id#}
-                                (concat
-                                 (for [[field# [type# opts#]] ~fields]
-                                   (let [namespaced-field#  (keyword ~(:namespace entity) field#)
-
-                                         defaults# ~(:defaults entity)
-
-                                         val# (cond (opts# :assignment-required)
-                                                    (get input# (keyword field#))
-
-                                                    (and (opts# :assignment-permitted) (contains? input# (keyword field#)))
-                                                    (get input# (keyword field#))
-
-                                                    (contains? defaults# field#)
-                                                    (let [default# (get defaults# field#)]
-                                                      (if (fn? default#)
-                                                        (default#)
-                                                        default#)))]
-                                     (when (not (nil? val#))
-                                       [namespaced-field# val#])))
-                                 (for [field# (keys ~backrefs)]
-                                   (when-let [val# (get input# field#)]
-                                     [field# val#]))))
-                          [:db/add (d/tempid :db.part/tx) ~(keyword (:namespace entity) "txCreated") id#] ;;annotate the transactionx
-                          )))})))
 (defn ->create
   "The `create!` function for a given entity"
   [entity pull]
