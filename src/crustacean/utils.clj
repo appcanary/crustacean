b3cfc4ecc144ac796652c91fe6bbe1d64fb4574c
Max Veytsman <maxim@ontoillogical.com>
2016-01-18 17:46:39 -0500

    Rewrote migration printer
    Added defn-db to crustacean (from canary)

--- src/crustacean/utils.clj
+++ src/crustacean/utils.clj
@@ -1,4 +1,5 @@
-(ns crustacean.utils)
+(ns crustacean.utils
+  (:require [puget.printer :as puget]))
 
 (defn entity-exists?
   "Checks that an entity returned by d/pull actually exists"
@@ -54,9 +55,49 @@
           (fields-with model :unique-identity)))
 
 (defn spit-edn
-  "Writes a form to file regardless of its size"
+  "Writes a form to file. We pretty-print with special care to render datomic objects correctly"
   [filepath data]
   (with-open [w (clojure.java.io/writer filepath)]
     (binding [*print-length* false
               *out* w]
-      (pr data))))
+      (puget/pprint data {:print-handlers {datomic.db.DbId puget/pr-handler
+                                           datomic.function.Function puget/pr-handler}
+                          :sort-keys false
+                          :width 100}))))
+
+
+(defmacro defn-db
+  "Defines a database function"
+  [name & body]
+  (let [doc (when (string? (first body))
+              (first body))
+        body (if doc (rest body) body)
+        opts (when (map? (first body))
+               (first body))
+        body (if opts (rest body) body)
+        params (first body)
+        body (rest body)
+        conds (when (and (next body) (map? (first body)))
+                (first body))
+        body (if conds (next body) body)
+        pre (:pre conds)
+        post (:post conds)
+        body (if post
+               `((let [~'% ~(if (< 1 (count body))
+                              `(do ~@body)
+                              (first body))]
+                   ~@(map (fn* [c] `(assert ~c)) post)
+                   ~'%))
+               body)
+        body (if pre
+               (concat (map (fn* [c] `(assert ~c)) pre)
+                       body)
+               body)]
+    (if (not (vector? params))
+      (throw (IllegalArgumentException. "Parameter declaration should be a vector")))
+    `(def
+       ~(vary-meta name assoc :doc doc)
+       (d/function (merge ~opts
+                          '{:lang :clojure
+                            :params ~params
+                            :code (do ~@body)})))))
