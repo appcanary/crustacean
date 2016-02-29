16b89f8d0113ad4a9930b49a170e0d2ee2c02ffd
Phill MV <phillmv@state.io>
2016-02-29 17:10:18 -0500

    Added documenting comment to crustacean.migrations/sync-all-models

--- src/crustacean/migrations.clj
+++ src/crustacean/migrations.clj
@@ -236,6 +236,8 @@
         sorted-migrations (sort-by #(.parse date-format (:date (second %))) all-migrations)]
     (doseq [[migration-name migration] sorted-migrations]
       ;; Make sure we sync schema up to every migration
+      ;; Occasionally, we have to wait for a given migration to conclude
+      ;; prior to running the next one.
       (when-let [db-after (:db-after (try (deref (c/ensure-conforms conn {migration-name migration}))
                                                   (catch Throwable e nil)))]
         (d/sync-schema conn (d/basis-t db-after))))))
